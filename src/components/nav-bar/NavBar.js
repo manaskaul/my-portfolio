f1dd4bad17c6711a9036a424a3faa72eddb7908d
Manas Kaul <vause@Manass-MacBook-Air.local>
2024-01-11 19:36:30 +0530

    fix imports

--- src/components/nav-bar/NavBar.js
+++ src/components/nav-bar/NavBar.js
@@ -1,6 +1,6 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./NavBar.css";
-import { Link, Router } from "react-router-dom";
 
 export default function NavBar() {
   return (
